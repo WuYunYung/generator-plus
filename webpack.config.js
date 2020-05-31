bc3ddfb6b4a4aca2476799589a633021bdc55c92
wilson.wu <justhurr@outlook.com>
2020-05-31 16:08:25 +0800

    编译成es5

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.js',//入口文件
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),//出口路径
+    filename: 'main.js',//出口文件
+    publicPath:'dist/'//静态资源路径
   },
   module: {
     rules: [
       {
         test:/\.css$/,
         use:['style-loader','css-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ] ,
   }
