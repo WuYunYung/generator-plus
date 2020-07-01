1ee7bf40c77c692502988acc0630a18b894e9a0d
WilsonWu <wilson.wu@kantar.com>
2020-07-01 19:00:31 +0800

    linkItem布局更新

--- src/components/linkItem.vue
+++ src/components/linkItem.vue
@@ -1,24 +1,54 @@
 <template>
-  <div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">{{cname}}</h5>
+      <div class="btn btn-secondary">show</div>
+      <div class="btn btn-secondary">go</div>
+      <div class="btn btn-secondary">copy</div>
+    </div>
     <div class="form-group animated fadeIn" v-if="clink!='#'">
-      <span>{{cname}}:</span>
       <div class="input-group input-group-sm">
-        <input :id="cid" type="text" class="form-control" :value="clink" readonly />
+        <div class="input-group-prepend">
+          <button class="btn btn-outline-secondary">{{cname}}</button>
+        </div>
+        <input
+          :id="cid"
+          type="text"
+          class="form-control"
+          :value="clink"
+          readonly
+          style="display:none"
+        />
         <div class="input-group-append">
-          <a class="btn btn-outline-secondary" :href="clink" target="_blank">Go</a>
+          <a class="btn btn-outline-secondary" :href="clink" target="_blank"><i class="fa fa-arrow-right"> Go</i></a>
           <button
             class="btn btn-outline-secondary clipboard"
             :data-clipboard-target="'#'+cid"
             type="button"
             title="Copy!"
           >
-            <i class="fa fa-copy"></i>
+            <i class="fa fa-copy"> Copy</i>
           </button>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.input-group{
+  display: flex;
+  flex-direction: row;
+  justify-content: right;
+}
+.input-group-prepend{
+  flex:1;
+  text-align:left;
+}
+.input-group-prepend *{
+  width: 100%;
+}
+</style>
 
 <script>
 export default {
