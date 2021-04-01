7dc2174f72e9897d0f0269e4ecb6791e07159000
WilsonWu <justhurr@outlook.com>
2021-04-01 11:42:45 +0800

    调整目录和文件命名

--- src/router/index.js
+++ src/router/index.js
@@ -1,48 +1,48 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const Projects = ()=>import('../views/projects')
-const Home =()=>import('../views/Home')
-const About =()=>import('../views/About')
+const Projects = () => import("../views/projects");
+const Home = () => import("../views/Home");
+const About = () => import("../views/About");
 
 const routes = [
   {
-    path: '/projects',
-    component:Projects,
+    path: "/projects",
+    component: Projects,
     children: [
       {
-        path: '/projects/:sn',
-        component: ()=>import('../views/projects/project'),
-        children:[
+        path: "/projects/:sn",
+        component: () => import("../views/projects/project"),
+        children: [
           {
-            path:'/projects/:sn/urls',
-            component:()=>import('../views/projects/SurveyURLs')
+            path: "/projects/:sn/urls",
+            component: () => import("../views/projects/surveyURLs"),
           },
           {
-            path:'/projects/:sn/others',
-            component:()=>import('../views/projects/OtherURLs')
-          }
-        ]
-      }
-    ]
+            path: "/projects/:sn/others",
+            component: () => import("../views/projects/otherURLs"),
+          },
+        ],
+      },
+    ],
   },
   {
-    path: '/Home',
-    alias: '/',
-    component:Home,
-  },{
-    path:'/About',
-    component:About,
-  }
-]
-
+    path: "/Home",
+    alias: "/",
+    component: Home,
+  },
+  {
+    path: "/About",
+    component: About,
+  },
+];
 
 const router = new VueRouter({
   routes,
-  mode: 'history',
-  linkActiveClass: 'active'
-})
+  mode: "history",
+  linkActiveClass: "active",
+});
 
-export default router
+export default router;
